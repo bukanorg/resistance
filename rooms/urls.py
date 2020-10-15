507f5a5019772ced27478da36a3f8b99c8a7ddf4
otaconisme <otaconisme@gmail.com>
2020-10-15 18:34:40 +0800

    lobby page

---
+++ rooms/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'rooms'
+
+urlpatterns = [
+    path('', views.lobby, name='lobby'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('setcookie/<str:cookie>/', views.store_cookie, name='setcookie'),
+    # path('getcookie/', views.get_cookie, name='getcookie'),
+]
